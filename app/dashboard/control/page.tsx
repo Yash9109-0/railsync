afa7f34a8ccd0e5d521d86467d2d69c208163d02
field <field@railsync.demo>
2026-09-14 19:28:01 +0530

    fix: timetable UI polish - single header, live badges, train cards

--- app/dashboard/control/page.tsx
+++ app/dashboard/control/page.tsx
@@ -1016,9 +1016,6 @@ useEffect(() => {
           </div>
 
           <Card>
-            <CardHeader>
-              <CardTitle>Live Corridor View - Multi-Track</CardTitle>
-            </CardHeader>
             <CardContent>
               <LiveTrackMap timetable={timetable} />
             </CardContent>
